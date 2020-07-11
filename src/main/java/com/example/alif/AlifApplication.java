c3fc592365132111d3b3f55ac39c7319d228f392
alif462 <mualifdwisaputra285@gmail.com>
2020-07-12 02:46:09 +0700

    Uas

---
+++ src/main/java/com/example/alif/AlifApplication.java
@@ -0,0 +1,13 @@
+package com.example.alif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlifApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/alif/repo/DataMhsRepo.java
@@ -0,0 +1,9 @@
+package com.example.alif.repo;
+
+import com.example.alif.entity.DataMhs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DataMhsRepo extends JpaRepository<DataMhs, String> {
+}
